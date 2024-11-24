99ceb20f0467feeafadd56b2888df2ac65bacf93
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-11-24 01:27:12 -0300

    feat: enhance contrast

--- jetson-nano/lane_detection.py
+++ jetson-nano/lane_detection.py
@@ -2,6 +2,14 @@ import cv2
 import numpy
 
 def preprocessing_pipeline(frame, blur_kernel_size=3, canny_low_threshold=50, canny_high_threshold=200):
+    l, a, b = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2LAB))
+
+    clahe = cv2.createCLAHE(clipLimit=2., tileGridSize=(8, 8))
+
+    l = clahe.apply(l)
+
+    frame = cv2.cvtColor(cv2.merge((l, a, b)), cv2.COLOR_LAB2BGR)
+
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     frame = cv2.GaussianBlur(frame, (blur_kernel_size, blur_kernel_size), 0)
